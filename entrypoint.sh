8c2a215672476fe402f831ce5b9dbe0b09ea4b0d
ket4yii <ket4yiit@gmail.com>
2017-02-04 22:52:31 +0300

    Add config file existring checking.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 export LOCAL_DB_HOST=$(echo $DB_HOST | awk -F ":" '{ print $1 }')
 export LOCAL_DB_PORT=$(echo $DB_HOST | awk -F ":" '{ if ($2 == "") print 3306; else $2; }')
 
-envsubst < /config.tmpl.yml > ./app/config.yml
+[ ! -f ./app/config.yml ] envsubst < /config.tmpl.yml > ./app/config.yml
 ./bin/console php-censor:migrate
 nginx
 php-fpm
