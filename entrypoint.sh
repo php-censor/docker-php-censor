40c9eba9fa402c3802ee61c9540d1a4d634d60ce
ket4yii <ket4yiit@gmail.com>
2017-02-04 22:47:10 +0300

    Codestyle fixes. Add db initialization.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@
 export LOCAL_DB_HOST=$(echo $DB_HOST | awk -F ":" '{ print $1 }')
 export LOCAL_DB_PORT=$(echo $DB_HOST | awk -F ":" '{ if ($2 == "") print 3306; else $2; }')
 
-CENSOR_DIR=/var/www/html
-
-envsubst < /config.tmpl.yml > $CENSOR_DIR/app/config.yml
+envsubst < /config.tmpl.yml > ./app/config.yml
+./bin/console php-censor:migrate
 nginx
 php-fpm
