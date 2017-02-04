cbf919c18f19daec8dcf0a6c866eb0eb3c70988d
ket4yii <ket4yiit@gmail.com>
2017-02-04 22:47:45 +0300

    Add pipefail in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -eo pipefail
 
 export LOCAL_DB_HOST=$(echo $DB_HOST | awk -F ":" '{ print $1 }')
 export LOCAL_DB_PORT=$(echo $DB_HOST | awk -F ":" '{ if ($2 == "") print 3306; else $2; }')
