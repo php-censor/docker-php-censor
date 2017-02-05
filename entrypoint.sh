4402cb8d0a7b18c83b0757d5c616e6b4a964a516
ket4yii <ket4yiit@gmail.com>
2017-02-05 15:13:23 +0300

    [entrypoint] Fix with default port value to empty string.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 export LOCAL_DB_HOST=$(echo $DB_HOST | awk -F ":" '{ print $1 }')
-export LOCAL_DB_PORT=$(echo $DB_HOST | awk -F ":" '{ if ($2 == "") print 3306; else $2; }')
+export LOCAL_DB_PORT=$(echo $DB_HOST | awk -F ":" '{ print $2; }')
 
 [ ! -f ./app/config.yml ] && envsubst < /config.tmpl.yml > ./app/config.yml
 ./bin/console php-censor:migrate
