fd98f5982467dc3724eba10c8c13a9be67d1b212
Dmitry Khomutov <poisoncorpsee@gmail.com>
2017-02-12 12:45:05 +0700

    Added MySQL support

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -4,6 +4,8 @@ set -eo pipefail
 export LOCAL_DB_HOST=$(echo $DB_HOST | awk -F ":" '{ print $1 }')
 export LOCAL_DB_PORT=$(echo $DB_HOST | awk -F ":" '{ print $2; }')
 
+sleep 20
+
 [ ! -f ./app/config.yml ] && envsubst < /config.tmpl.yml > ./app/config.yml
 ./bin/console php-censor:install --config-from-file=yes --admin-name=$ADMIN_NAME --admin-password=$ADMIN_PASSWORD --admin-email=$ADMIN_EMAIL
 nginx
