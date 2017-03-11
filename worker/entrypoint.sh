a0bc69b5a1418fcba96c02177a1c918a1f8d3b4a
ket4yii <ket4yiit@gmail.com>
2017-03-11 04:18:01 +0300

    [worker] Add waiting and change db vars name

--- worker/entrypoint.sh
+++ worker/entrypoint.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 set -eo pipefail
 
-export LOCAL_DB_HOST=$(echo $DB_HOST | awk -F ":" '{ print $1 }')
-export LOCAL_DB_PORT=$(echo $DB_HOST | awk -F ":" '{ print $2; }')
+wait_for_db() 
+{
+    while nc -w 2 -v $LOCAL_DB_HOST $LOCAL_DB_PORT < /dev/null 2> /dev/null &>1
+    do
+      sleep 1;
+    done
+}
+
+set_args() {
+    LOCAL_DB_HOST=$DB_HOST
+
+    export DB_HOST=$(echo $LOCAL_DB_HOST | awk -F ":" '{ print $1 }')
+    export DB_PORT=$(echo $LOCAL_DB_HOST | awk -F ":" '{ print $2; }')
+}
+
+set_args
 
 [ ! -f ./app/config.yml ] && envsubst < /config.tmpl.yml > ./app/config.yml
+
+wait_for_db
 
 if [ -f /docker-init.d/install.sh ]
 then
