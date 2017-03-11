733fe951bda5c2394e24a56c8a9cf7800bfae299
ket4yii <ket4yiit@gmail.com>
2017-03-11 23:57:27 +0300

    [entrypoint] Code style fixes

--- worker/entrypoint.sh
+++ worker/entrypoint.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 set -eo pipefail
 
-wait_for_external_services()
-{
-    while ! ( nc -w 2 -v "$DB_HOST" "$DB_PORT" < /dev/null && nc -w 2 -v "$BEANSTALK_HOST" 11300 < /dev/null )
+# Check external services (db, queue) on ready
+wait_for_external_services() {
+    local BEANSTALK_DEFAULT_PORT="11300"
+
+    while ! ( nc -w 2 -v "$DB_HOST" "$DB_PORT" < /dev/null && nc -w 2 -v "$BEANSTALK_HOST" $BEANSTALK_DEFAULT_PORT < /dev/null )
     do
       echo "Wait for db and queue"
     done
 }
 
-set_args() {
-    LOCAL_DB_HOST=$DB_HOST
+# Process DB args and set it to global
+parse_args() {
+    local NON_PARSED_DB_HOST=$DB_HOST
 
-    export DB_HOST=$(echo "$LOCAL_DB_HOST" | awk -F ":" '{ print $1 }')
-    export DB_PORT=$(echo "$LOCAL_DB_HOST" | awk -F ":" '{ if (ENVIRON["DB_PORT"] != "") print ENVIRON["DB_PORT"]; else print $2 }')
+    export DB_HOST=$(echo "$NON_PARSED_DB_HOST" | awk -F ":" '{ print $1 }')
+    export DB_PORT=$(echo "$NON_PARSED_DB_HOST" | awk -F ":" '{ if (ENVIRON["DB_PORT"] != "") print ENVIRON["DB_PORT"]; else print $2 }')
 
     if [ -z "$DB_PORT" ]; then
         case "$DB_TYPE" in
@@ -27,17 +30,23 @@ set_args() {
     fi
 }
 
-set_args
+# Entrypoint
+main() {
+    parse_args
+    
+    if [ ! -f ./app/config.yml ]; then
+        envsubst < /config.tmpl.yml > ./app/config.yml
+    fi
+    
+    wait_for_external_services
 
-[ ! -f ./app/config.yml ] && envsubst < /config.tmpl.yml > ./app/config.yml
+    if [ -f /docker-init.d/install.sh ]; then
+        echo "Install your dependencies..."
+        . /docker-init.d/install.sh
+        echo "Installing is done."
+    fi
+    
+    ./bin/console php-censor:worker
+}
 
-wait_for_external_services
-
-if [ -f /docker-init.d/install.sh ]
-then
-    echo "Install your dependencies"
-    . /docker-init.d/install.sh
-    echo "Installing is done."
-fi
-
-./bin/console php-censor:worker
+main
